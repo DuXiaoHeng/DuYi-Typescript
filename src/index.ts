b9526b92a7373fea4b2e6da41b1d29b0cef2014e
yuanjin <yuanjinmoke@gmail.com>
2019-04-23 17:01:33 +0800

    3. 基本类型检查 - 练习：创建并打印扑克牌

--- src/index.ts
+++ src/index.ts
@@ -1,119 +1,56 @@
-// function isOdd(n: number) {
-//     return n % 2 === 0;
-// }
-
-// let nums: number[] = [3, 4, 5]
-
-// let nums: Array<number> = [3, 4, 5];
-
-// function printValues(obj: object) {
-//     const vals = Object.values(obj)
-//     vals.forEach(v => console.log(v));
-// }
-
-// printValues({
-//     name:"afd",
-//     age:33
-// })
-
-// let n:string = undefined;
-
-// n.toUpperCase();
-
-// let name: string | undefined;
-
-// function printMenu(){
-//     console.log("1. 登录")
-//     console.log("2. 注册");
-// }
-
-// function throwError(msg: string): never {
-//     throw new Error(msg);
-// }
-
-// function alwaysDoSomething(): never {
-//     while (true) {
-//         //...
-//     }
-// }
-
-// let gender: "男" | "女";
-
-// gender = "女";
-
-// gender = "男";
-
-// let arr: []; //arr永远只能取值为一个空数组
-
-// let user: {
-//     name:string
-//     age:number
-// }
-
-// user = {
-//     name:"34",
-//     age:33
-// }
-
-// let tu: [string, number];
-
-// tu = ["3", 4];
-
-// let data:any = "sfdsdf";
-
-// let num:number = data;
-// type Gender = "男" | "女"
-// type User = {
-//     name:string
-//     age:number
-//     gender:Gender
-// }
-
-// let u:User
-
-// u = {
-//     name:"sdfd",
-//     gender:"男",
-//     age:34
-// }
-
-// function getUsers(g:Gender):User[] {
-//     return [];
-// }
-
-// /**
-//  * 得到a*b的结果
-//  * @param a 
-//  * @param b 
-//  */
-// function combine(a:number, b:number):number;
-// /**
-//  * 得到a和b拼接的结果
-//  * @param a 
-//  * @param b 
-//  */
-// function combine(a:string, b:string):string;
-// function combine(a: number | string, b: number | string): number | string {
-//     if (typeof a === "number" && typeof b === "number") {
-//         return a * b;
-//     }
-//     else if (typeof a === "string" && typeof b === "string") {
-//         return a + b;
-//     }
-//     throw new Error("a和b必须是相同的类型");
-// }
-
-// const result = combine("a","b")
-
-function sum(a: number, b: number, c: number) {
-    // if (c) {
-    //     return a + b + c;
-    // }
-    // else {
-    //     return a + b;
-    // }
+type Deck = NormalCard[]
+type Color = "♥" | "♠" | "♦" | "♣";
+type NormalCard = {
+    color: Color
+    mark: number
 }
 
-// sum(3, 4);
+function createDeck(): Deck {
+    const deck: Deck = [];
+    for (let i = 1; i <= 13; i++) {
+        deck.push({
+            mark: i,
+            color: "♠"
+        })
+        deck.push({
+            mark: i,
+            color: "♣"
+        })
+        deck.push({
+            mark: i,
+            color: "♥"
+        })
+        deck.push({
+            mark: i,
+            color: "♦"
+        })
+    }
+    return deck;
+}
 
-// sum(3, 4, 5);
+function printDeck(deck: Deck) {
+    let result = "\n";
+    deck.forEach((card, i) => {
+        let str = card.color;
+        if (card.mark <= 10) {
+            str += card.mark;
+        }
+        else if (card.mark === 11) {
+            str += "J";
+        }
+        else if (card.mark === 12) {
+            str += "Q";
+        }
+        else {
+            str += "K";
+        }
+        result += str + "\t";
+        if ((i + 1) % 6 === 0) {
+            result += "\n";
+        }
+    })
+    console.log(result);
+}
+
+const deck = createDeck();
+printDeck(deck);
