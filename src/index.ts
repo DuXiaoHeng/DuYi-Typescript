e320fecdab3a49a97fc1b68a39544d8e25b2f9a0
yuanjin <yuanjinmoke@gmail.com>
2019-04-26 16:31:25 +0800

    8. 泛型 - 自定义字典类

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,21 @@
-// interface hasNameProperty {
-//     name: string
-// }
+import { Dictionary } from "./dictionary";
 
-// /**
-//  * 将某个对象的name属性的每个单词的首字母大小，然后将该对象返回
-//  */
-// function nameToUpperCase<T extends hasNameProperty>(obj: T): T {
-//     obj.name = obj.name
-//         .split(" ")
-//         .map(s => s[0].toUpperCase() + s.substr(1))
-//         .join(" ");
-//     return obj;
-// }
+const dic = new Dictionary<string, number>();
 
-// const o = {
-//     name:"kevin yuan",
-//     age:22,
-//     gender:"男"
-// }
+dic.set("a", 1);
+dic.set("b", 2);
+dic.set("a", 11);
+dic.set("c", 33);
 
-// const newO = nameToUpperCase(o);
+dic.forEach((k, v) => {
+    console.log(`${k}:${v}`);
+})
+console.log("当前键值对数量：" + dic.size);
+console.log("删除键b")
 
-// console.log(newO.name); //Kevin Yuan
+dic.delete("b");
 
-
-//将两个数组进行混合
-//[1,3,4] + ["a","b","c"] = [1, "a", 3, "b", 4, "c"]
-function mixinArray<T, K>(arr1: T[], arr2: K[]): (T | K)[] {
-    if (arr1.length != arr2.length) {
-        throw new Error("两个数组长度不等");
-    }
-    let result: (T | K)[] = [];
-    for (let i = 0; i < arr1.length; i++) {
-        result.push(arr1[i]);
-        result.push(arr2[i]);
-    }
-    return result;
-}
-
-const result = mixinArray([1, 3, 4], ["a", "b", "c"]);
-
-result.forEach(r => console.log(r));
+dic.forEach((k, v) => {
+    console.log(`${k}:${v}`);
+})
+console.log("当前键值对数量：" + dic.size);
