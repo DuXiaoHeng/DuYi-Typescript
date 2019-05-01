c62c36540b575f2eb1e3f3306f19d678b3c4cda1
yuanjin <yuanjinmoke@gmail.com>
2019-05-01 14:09:43 +0800

    this指向约束

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-class B{
+// interface IUser {
+//     name: string,
+//     age: number,
+//     sayHello(this: IUser): void
+// }
 
+// const u: IUser = {
+//     name: "ssf",
+//     age: 33,
+//     sayHello() {
+//         console.log(this.name, this.age)
+//     }
+// }
+// const say = u.sayHello;
+
+class User {
+    constructor(
+        public name: string,
+        public age: number
+    ) { }
+
+    sayHello() {
+        console.log(this, this.name, this.age)
+    }
 }
-
-class A {
-    [prop: number]: B
-    [prop: string]: object
-}
-
