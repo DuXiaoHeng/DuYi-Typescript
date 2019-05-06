ff96ced27f5a36d7df50b14d31ca42bdb3728877
yuanjin <yuanjinmoke@gmail.com>
2019-05-06 11:57:32 +0800

    进阶-3.装饰器-成员装饰器

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-@xxxx
-class User {
-    @require
-    @range(3,5)
-    @description("账号")
-    loginid: string; //描述是：账号，验证规则：1.必填，2.必须是3-5个字符
-
-
-    loginpwd: string; //必须是6-12个字符
-    age: number; //必须是0-100之间的数字
-    gender: "男" | "女";
+function enumerable(target: any, key: string, descriptor: PropertyDescriptor) {
+    // console.log(target, key, descriptor);
+    descriptor.enumerable = true;
 }
 
-class Article {
-    title: string; //长度必须是4-20个字符
-}
-
-/**
- * 统一的验证函数
- * @param obj 
- */
-function validate(obj: object) {
-    for (const key in obj) {
-        const val = (obj as any)[key];
-        //缺少该属性的验证规则
+function useless(target: any, key: string, descriptor: PropertyDescriptor) {
+    descriptor.value = function () {
+        console.warn(key + "方法已过期");
     }
 }
+
+class A {
+
+
+    @enumerable
+    @useless
+    method1() {
+        console.log("method1");
+    }
+
+    @enumerable
+    method2() {
+
+    }
+}
+
+const a = new A();
+a.method1();
