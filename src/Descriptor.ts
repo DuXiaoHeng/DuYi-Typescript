8ccb2a15957e848bcb8022cedbc8ae90c504a102
yuanjin <yuanjinmoke@gmail.com>
2019-05-06 14:35:40 +0800

    reflect-metadata

--- src/Descriptor.ts
+++ src/Descriptor.ts
@@ -1,45 +1,27 @@
+import "reflect-metadata";
 
-export function classDescriptor(description: string) {
-    return function (target: Function) {
-        //保存到该类的原型中
-        target.prototype.$classDescription = description;
-    }
-}
+const key = Symbol.for("descriptor");
 
-export function propDescriptor(description: string) {
-    return function (target: any, propName: string) {
-        //把所有的属性信息保存到该类的原型中
-        if (!target.$propDescriptions) {
-            target.$propDescriptions = [];
-        }
-        target.$propDescriptions.push({
-            propName,
-            description
-        });
-    }
+export function descriptor(description: string) {
+    return Reflect.metadata(key, description)
 }
 
 export function printObj(obj: any) {
+    const cons = Object.getPrototypeOf(obj);
     //输出类的名字
-    if (obj.$classDescription) {
-        console.log(obj.$classDescription);
+    if (Reflect.hasMetadata(key, cons)) {
+        console.log(Reflect.getMetadata(key, cons));
     }
     else {
-        console.log(Object.getPrototypeOf(obj).constructor.name);
-    }
-    if (!obj.$propDescriptions) {
-        obj.$propDescriptions = [];
+        console.log(cons.constructor.name);
     }
     //输出所有的属性描述和属性值
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            const prop = obj.$propDescriptions.find((p: any) => p.propName === key);
-            if (prop) {
-                console.log(`\t${prop.description}:${obj[key]}`)
-            }
-            else {
-                console.log(`\t${key}:${obj[key]}`)
-            }
+    for (const k in obj) {
+        if (Reflect.hasMetadata(key, obj, k)) {
+            console.log(`\t${Reflect.getMetadata(key, obj, k)}:${obj[k]}`)
+        }
+        else {
+            console.log(`\t${k}:${obj[k]}`)
         }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { classDescriptor, propDescriptor, printObj } from "./Descriptor";
+import { printObj, descriptor } from "./Descriptor";
 
-@classDescriptor("文章")
+@descriptor("文章")
 class Article {
-    
-    @propDescriptor("标题")
-    title:string
 
-    @propDescriptor("内容")
-    content:string
+    @descriptor("标题")
+    title: string
 
-    @propDescriptor("日期")
-    date:Date
+    @descriptor("内容")
+    content: string
+
+    @descriptor("日期")
+    date: Date
 }
 
 const ar = new Article();
@@ -19,3 +19,20 @@ ar.content = "asdfasdfasdfasdfasdf";
 ar.date = new Date();
 
 printObj(ar);
+
+// import "reflect-metadata";
+
+// @Reflect.metadata("a1", "asdfasdfasdfaf")
+// @Reflect.metadata("a2", "adsfasf")
+// @Reflect.metadata("a", "一个类")
+// class A {
+
+//     @Reflect.metadata("a", "一个属性")
+//     prop1:string
+// }
+
+// const obj = new A();
+
+// console.log(Reflect.getMetadata("a", A));
+
+// console.log(Reflect.getMetadata("a", obj, "prop1"))
