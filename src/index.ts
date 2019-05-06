7561a89bd1ef7781ff328ece244471bab0664f02
yuanjin <yuanjinmoke@gmail.com>
2019-05-06 10:24:17 +0800

    进阶-3.装饰器-概述

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
-// interface IUser {
-//     name: string,
-//     age: number,
-//     sayHello(this: IUser): void
-// }
-
-// const u: IUser = {
-//     name: "ssf",
-//     age: 33,
-//     sayHello() {
-//         console.log(this.name, this.age)
-//     }
-// }
-// const say = u.sayHello;
-
+@xxxx
 class User {
-    constructor(
-        public name: string,
-        public age: number
-    ) { }
+    @require
+    @range(3,5)
+    @description("账号")
+    loginid: string; //描述是：账号，验证规则：1.必填，2.必须是3-5个字符
 
-    sayHello() {
-        console.log(this, this.name, this.age)
+
+    loginpwd: string; //必须是6-12个字符
+    age: number; //必须是0-100之间的数字
+    gender: "男" | "女";
+}
+
+class Article {
+    title: string; //长度必须是4-20个字符
+}
+
+/**
+ * 统一的验证函数
+ * @param obj 
+ */
+function validate(obj: object) {
+    for (const key in obj) {
+        const val = (obj as any)[key];
+        //缺少该属性的验证规则
     }
 }
